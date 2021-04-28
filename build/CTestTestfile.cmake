ba911bcbcd3467eec7f0bcb1c4391d0a514d2939
LargeBee <rowant@firemail.cc>
2021-04-28 15:25:36 +0100

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rowan/dev/mahjonggl
-# Build directory: /home/rowan/dev/mahjonggl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

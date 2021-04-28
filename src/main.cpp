d523c7a9865f5f0014062768ededaef90878544e
LargeBee <rowant@firemail.cc>
2021-04-28 19:02:02 +0100

    new dev branch

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int, char**) {
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    } 
+    } //
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
